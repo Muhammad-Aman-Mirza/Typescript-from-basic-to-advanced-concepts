5c59cd52762677958aadd3770b66ba5637c80af8
DESKTOP-8IUR1AM\Glow Computers <aman.mirza9@yahoo.com>
2024-12-05 10:29:37 -0800

    feat: types work done

--- src/2-object-types/2-class.ts
+++ src/2-object-types/2-class.ts
@@ -1,11 +1,3 @@
-// src/index.ts
-const message: string = "Hello, TypeScript!";
-console.log(message); // This will print the message to the console when run
-
-// src/index.ts
-const message: string = "Hello, TypeScript!";
-console.log(message); // This will print the message to the console when run
-
 interface CarFeatures {
   hasSunroof: boolean;
   color: string;
