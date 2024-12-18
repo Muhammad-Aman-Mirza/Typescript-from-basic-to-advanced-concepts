e659a3082568e892735027b2bc2cc765a2dbb622
DESKTOP-8IUR1AM\Glow Computers <aman.mirza9@yahoo.com>
2024-12-18 00:36:26 -0800

    feat: working on externel modules

---
+++ src/9-typescript-modules/3-externel-modules.ts
@@ -0,0 +1,11 @@
+/**
+ ** External Modules
+ ** In TypeScript, external modules allow you to organize and share code across multiple files.
+ ** External modules in TypeScript follow the CommonJS or ES modules standards.
+ */
+export function doSomething() {
+  console.log("Doing something...");
+}
+
+// import { doSomething } from "./9-typescript-modules/3-externel-modules";
+// doSomething();
